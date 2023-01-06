295808d79b1a79365543695d2878073def4cbc2a
Sebastian Tabares <sytabaresa@gmail.com>
2023-01-05 19:18:54 -0500

    fix CDN domain

--- config.production.js
+++ config.production.js
@@ -17,7 +17,7 @@ module.exports = {
             },
         },
     },
-    baseURL: 'https://tunnel.alire.co/',
+    baseURL: 'https://personal-signature.pages.dev/',
     inlineCSS: true,
     removeUnusedCSS: true,
 }
